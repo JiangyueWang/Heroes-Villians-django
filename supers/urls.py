241f90297e191c6e55f87ee06207005e19bc2210
Jiang <jiangyue0380@gmail.com>
2022-08-26 00:51:15 +1000

    updated path for super app + created supers_list and super_details function in views.py for supers app + updated Super model in supers app

--- supers/urls.py
+++ supers/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from supers import views
 urlpatterns = [
     path('api/supers/', views.supers_list),
+    path('api/supers/<int:pk>', views.super_details)
 
 ]
--- supers/views.py
+++ supers/views.py
@@ -1,8 +1,24 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from supers.models import Super
+from supers.serializer import SuperSerializer
 # Create your views here.
 
 
 @api_view(['POST'])
-def supers_list(requst):
-    return Response('Hello World')
+def supers_list(request):
+    if request.method == 'POST':
+        serializer_super = SuperSerializer(data=request.data)
+        serializer_super.is_valid(raise_exception=True)
+        serializer_super.save()
+
+    return Response(serializer_super.data, status=status.HTTP_201_CREATED)
+
+
+@api_view(['GET'])
+def super_details(requst, pk):
+    requested_super = Super.objects.get(pk=pk)
+    if requst.method == 'GET':
+        serializer_super = SuperSerializer(requested_super)
+        return Response(serializer_super.data, status=status.HTTP_200_OK)
